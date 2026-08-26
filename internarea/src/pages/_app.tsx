ac396a72ea5e1b1f66bff9441d3720ed6451aeb0
Adwaith <justmad1811@gmail.com>
2026-08-26 16:57:21 +0530

    Prevent AuthListener from logging out admins on firebase null state

--- internarea/src/pages/_app.tsx
+++ internarea/src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { Provider, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { auth } from "@/firebase/firebase";
 import { login, logout } from "@/Feature/Userslice";
+import { isAdmin } from "@/lib/auth";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 export default function App({ Component, pageProps }: AppProps) {
@@ -24,7 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
               phoneNumber: authuser.phoneNumber,
             })
           );
-        } else {
+        } else if (!isAdmin()) {
           dispatch(logout());
         }
       });
